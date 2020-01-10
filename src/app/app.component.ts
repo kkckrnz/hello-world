18756cf90256ce7c3c7b86c989048e14df116973
Ken Choi <kkckrnz@gmail.com>
2020-01-10 18:00:02 +1300

    Component Interaction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'This is AppComponent';
+  public name = "Ken";
+  public message = "";
 }
--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// Be careful here, because EventEmitter is automatically imported from protractor and that gives me an error.
+// import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-test',
@@ -7,11 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
-  public colors = ["red", "blue", "green", "yellow"];
+  // @Input() public parentData;
+  // Or this can be done using alias
+  @Input('parentData') public name;
+
+  @Output() public childEvent = new EventEmitter();
 
   constructor() { }
 
   ngOnInit() {
   }
-    
+  
+  fireEvent() {
+    this.childEvent.emit("Hey this is a message from child component");
+  }
 }
